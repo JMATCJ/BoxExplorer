fb39cca47fa26dc9c8ec03c99d1247f026ed75f0
Jack Bergman <jrbergma@mtu.edu>
2018-12-01 14:37:06 -0500

    Made it so that players and enemies can no longer leave the board

--- src/main/java/com/github/jmatcj/ld43/entity/Enemy.java
+++ src/main/java/com/github/jmatcj/ld43/entity/Enemy.java
@@ -37,12 +37,24 @@ public class Enemy implements Entity {
 		// - Make is so that they cannot leave the boarder
 		// - Make it so when the enemy is shot it dies
 		// - Make it so there are multiple enemies
-		// - Make them move in a better manner
+		// - Make them move in a better manner so its not trash
 		
 		if (i == 10) {
 			xPos += (Math.random() * 50) + (Math.random() * -50);
 			yPos += (Math.random() * 50) + (Math.random() * -50);
-			//g.addListener(new Projectile(xPos, yPos, xPos, yPos, 20.0));
+			if (xPos > 1260) {
+				xPos = 1260;
+			}
+			if (xPos < 0) {
+				xPos = 0;
+			}
+			if (yPos > 700) {
+				yPos = 700;
+			}
+			if (yPos < 0) {
+				yPos = 0;
+			}
+			//g.addListener(new Projectile(xPos, yPos, xPos, yPos, 20.0)); //Let the enemy shoot
 			i = 0;
 		} else {
 			i++;
--- src/main/java/com/github/jmatcj/ld43/entity/Player.java
+++ src/main/java/com/github/jmatcj/ld43/entity/Player.java
@@ -28,15 +28,27 @@ public class Player implements Entity {
             switch(evt.getCode()) {
                 case W:
                     yPos -= velocity;
+                    if (yPos < 0) {
+                    	yPos = 0;
+                    }
                     break;
                 case A:
                     xPos -= velocity;
+                    if (xPos < 0) {
+                    	xPos = 0;
+                    }
                     break;
                 case S:
                     yPos += velocity;
+                    if (yPos > 700) {
+                    	yPos = 700;
+                    }
                     break;
                 case D:
                     xPos += velocity;
+                    if (xPos > 1260) {
+                    	xPos = 1260;
+                    }
                     break;
             }
         }
