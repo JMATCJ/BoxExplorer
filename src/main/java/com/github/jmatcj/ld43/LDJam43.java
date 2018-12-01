9570414e56150f945f045084b4fb18e3261c5781
Parker Young <parker@parker8283.net>
2018-11-30 21:27:08 -0500

    Initial Commit

---
+++ src/main/java/com/github/jmatcj/ld43/LDJam43.java
@@ -0,0 +1,26 @@
+package com.github.jmatcj.ld43;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LDJam43 extends Application {
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(LDJam43.class, args);
+    }
+}
