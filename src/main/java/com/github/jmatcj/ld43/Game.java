0a1ca36587950b291c22c045c01b3e55a26eea41
Trevor Good <tdgood@users.noreply.github.com>
2018-11-30 22:01:43 -0500

    Setting stage up

---
+++ src/main/java/com/github/jmatcj/ld43/Game.java
@@ -0,0 +1,8 @@
+package com.github.jmatcj.ld43;
+
+public class Game {
+    void update(long ns) {
+
+    }
+}
+
--- src/main/java/com/github/jmatcj/ld43/LDJam43.java
+++ src/main/java/com/github/jmatcj/ld43/LDJam43.java
@@ -1,23 +1,51 @@
 package com.github.jmatcj.ld43;
 
+import com.github.jmatcj.ld43.util.AssetLoader;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class LDJam43 extends Application {
+    private AnimationTimer gameLoop;
+    private Game game;
 
     @Override
     public void init() throws Exception {
-
+        AssetLoader.initialize(getParameters().getRaw().contains("-nomusic"));
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
+        Canvas canvas = new Canvas(1280, 720);
+        Scene scene = new Scene(new Group(canvas), 1270, 710);
+        primaryStage.setScene(scene);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                game.update(now);
+
+
+            }
+        };
+
+        gameLoop.start();
+
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     @Override
     public void stop() throws Exception {
-
+//        game.saveData();
+        gameLoop.stop();
     }
 
     public static void main(String[] args) {
