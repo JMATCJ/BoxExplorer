83dfff3238080c5a3fffa119b89e7ec011e6dd35
Parker Young <parker@parker8283.net>
2018-12-01 22:02:32 -0500

    Final change to the Map-generating algorithm (for now) Changed it to use a kind of adjacency matrix when building the map. Still not 100% happy with it, but I should probably be working on other things.

--- src/main/java/com/github/jmatcj/ld43/world/Map.java
+++ src/main/java/com/github/jmatcj/ld43/world/Map.java
@@ -25,25 +25,110 @@ public class Map {
             rooms.get(0).addAdjacentRoom(Room.Direction.VALUES[i - 1], rooms.get(i));
             rooms.get(i).addAdjacentRoom(Room.Direction.OPPOSITES[Room.Direction.VALUES[i - 1].ordinal()], rooms.get(0));
         }
-        int roomsSet = 5; // Number of rooms we have currently setup
-        int curBase = 1;
-        Room r1 = rooms.get(curBase); // Room we are currently operating on
-        while (roomsSet < numRooms) {
+        // A matrix to keep track of where rooms are in our grid
+        int[][] roomMatrix = new int[numRooms * 2 - 7][numRooms * 2 - 7];
+        // Set the starting room and the four rooms that are always adjacent to the starting room
+        roomMatrix[roomMatrix.length / 2][roomMatrix.length / 2] = -1;
+        roomMatrix[roomMatrix.length / 2 - 1][roomMatrix.length / 2] = 1;
+        roomMatrix[roomMatrix.length / 2 + 1][roomMatrix.length / 2] = 2;
+        roomMatrix[roomMatrix.length / 2][roomMatrix.length / 2 - 1] = 3;
+        roomMatrix[roomMatrix.length / 2][roomMatrix.length / 2 + 1] = 4;
+        int roomsSet = 5; // Number of rooms that have been set in the map
+        int row = roomMatrix.length / 2 - 1; // The current row we are indexing into
+        int col = roomMatrix.length / 2; // The current column we are indexing into
+        while (roomsSet < numRooms) { // While we haven't set all the rooms yet
             if (rng.nextBoolean()) { // Should we branch from this room
-                Room.Direction dir = Room.Direction.VALUES[rng.nextInt(4)];
-                if (r1.hasAdjacentRoom(dir)) { // Does this room already have a room adjacent in this direction
-                    r1 = r1.getAdjacentRoom(dir); // Start operating on that room
-                } else {
-                    r1.addAdjacentRoom(dir, rooms.get(roomsSet)); // Make it adjacent with the next room in the list
-                    rooms.get(roomsSet).addAdjacentRoom(Room.Direction.getOpposite(dir), r1);
-                    roomsSet++;
-                    curBase = (curBase % 4) + 1;
-                    r1 = rooms.get(curBase); // Set the current room to the one we just linked to, and increment the roomsSet count
+                Room.Direction dir = Room.Direction.VALUES[rng.nextInt(4)]; // Pick a random direction to branch from
+                int roomNum = roomMatrix[row][col] != -1 ? roomMatrix[row][col] : 0; // Get the room number for indexing into the list
+                if (rooms.get(roomNum).hasAdjacentRoom(dir)) { // Do we already have an adjacent room
+                    switch (dir) { // If so, recurse on that adjacent room
+                        case UP:
+                            row -= 1;
+                            break;
+                        case DOWN:
+                            row += 1;
+                            break;
+                        case LEFT:
+                            col -= 1;
+                            break;
+                        case RIGHT:
+                            col += 1;
+                            break;
+                    }
+                } else { // If we don't already have an explicit adjacent room
+                    // For each direction, first check if there is a room that is spatially adjacent, but no path has been formed yet
+                    //   If that is the case, form the path and recurse on that newly adjacent room
+                    // If there is no spatially adjacent room, place a new room there and recurse on that new room.
+                    switch (dir) {
+                        case UP:
+                            if (roomMatrix[row - 1][col] != 0) {
+                                int other = roomMatrix[row - 1][col] != -1 ? roomMatrix[row - 1][col] : 0;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.UP, rooms.get(other));
+                                rooms.get(other).addAdjacentRoom(Room.Direction.DOWN, rooms.get(roomNum));
+                                row -= 1;
+                            } else {
+                                roomMatrix[row - 1][col] = roomsSet;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.UP, rooms.get(roomsSet));
+                                rooms.get(roomsSet).addAdjacentRoom(Room.Direction.DOWN, rooms.get(roomNum));
+                                roomsSet++;
+                                row -= 1;
+                            }
+                            break;
+                        case DOWN:
+                            if (roomMatrix[row + 1][col] != 0) {
+                                int other = roomMatrix[row + 1][col] != -1 ? roomMatrix[row + 1][col] : 0;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.DOWN, rooms.get(other));
+                                rooms.get(other).addAdjacentRoom(Room.Direction.UP, rooms.get(roomNum));
+                                row += 1;
+                            } else {
+                                roomMatrix[row + 1][col] = roomsSet;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.DOWN, rooms.get(roomsSet));
+                                rooms.get(roomsSet).addAdjacentRoom(Room.Direction.UP, rooms.get(roomNum));
+                                roomsSet++;
+                                row += 1;
+                            }
+                            break;
+                        case LEFT:
+                            if (roomMatrix[row][col - 1] != 0) {
+                                int other = roomMatrix[row][col - 1] != -1 ? roomMatrix[row][col - 1] : 0;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.LEFT, rooms.get(other));
+                                rooms.get(other).addAdjacentRoom(Room.Direction.RIGHT, rooms.get(roomNum));
+                                col -= 1;
+                            } else {
+                                roomMatrix[row][col - 1] = roomsSet;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.LEFT, rooms.get(roomsSet));
+                                rooms.get(roomsSet).addAdjacentRoom(Room.Direction.RIGHT, rooms.get(roomNum));
+                                roomsSet++;
+                                col -= 1;
+                            }
+                            break;
+                        case RIGHT:
+                            if (roomMatrix[row][col + 1] != 0) {
+                                int other = roomMatrix[row][col + 1] != -1 ? roomMatrix[row][col + 1] : 0;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.RIGHT, rooms.get(other));
+                                rooms.get(other).addAdjacentRoom(Room.Direction.LEFT, rooms.get(roomNum));
+                                col += 1;
+                            } else {
+                                roomMatrix[row][col + 1] = roomsSet;
+                                rooms.get(roomNum).addAdjacentRoom(Room.Direction.RIGHT, rooms.get(roomsSet));
+                                rooms.get(roomsSet).addAdjacentRoom(Room.Direction.LEFT, rooms.get(roomNum));
+                                roomsSet++;
+                                col += 1;
+                            }
+                            break;
+                    }
                 }
-            } else { // Recurse back to the previous room we were in and try again
-                r1 = rooms.get(rng.nextInt(roomsSet));
-                //curBase = (curBase % 4) + 1;
-                //r1 = rooms.get(curBase); // Set the current room to the one we just linked to, and increment the roomsSet count
+            } else { // If we decided not to branch from here
+                // Find a random room and recurse on that one
+                // This is terribly inefficient, but I don't care.
+                // Our data size is small enough that it shouldn't matter too much.
+                int rand_row, rand_col;
+                do {
+                    rand_row = rng.nextInt(roomMatrix.length);
+                    rand_col = rng.nextInt(roomMatrix.length);
+                } while (roomMatrix[rand_row][rand_col] == 0);
+                row = rand_row;
+                col = rand_col;
             }
         }
     }
