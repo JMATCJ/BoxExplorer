250e7acdfc9a375a3920413529518f33d92d4150
Trevor Good <tdgood@users.noreply.github.com>
2018-12-01 23:50:57 -0500

    Projectile goes bye-bye after two bounces.

--- src/main/java/com/github/jmatcj/ld43/entity/Projectile.java
+++ src/main/java/com/github/jmatcj/ld43/entity/Projectile.java
@@ -8,6 +8,7 @@ import javafx.scene.paint.Color;
 public class Projectile extends Entity {
     private double dx;
     private double dy;
+    private int bounceCount = 0;
 
     public Projectile(double startX, double startY, double mouseX, double mouseY, double velocity) {
         super(startX, startY, 10, 10);
@@ -27,11 +28,16 @@ public class Projectile extends Entity {
         xPos += dx;
         yPos += dy;
 
+        if (bounceCount == 3) {
+            g.removeEntity(this);
+        }
         if (xPos < 0 || xPos > LDJam43.SCREEN_WIDTH - 10) {
             dx *= -1;
+            bounceCount++;
         }
         if (yPos < 0 || yPos > LDJam43.SCREEN_HEIGHT - 10) {
             dy *= -1;
+            bounceCount++;
         }
     }
 }
